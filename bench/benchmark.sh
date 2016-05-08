6a80d5b95c03c3d3bc67c0625e3b21f9db712adf
Paul Filitchkin <filitchp@gmail.com>
2016-05-08 16:02:49 -0700

    Additional tweaks to benchmarking

--- bench/benchmark.sh
+++ bench/benchmark.sh
@@ -1,17 +1,56 @@
 #!/bin/sh
 
 # Basic variation of sizes and algorithms
+echo
+echo '------------------------------------------------------------------------------------------'
+echo
+echo ' Benchmark 1: Three resize types on 3 output sizes. No sharpening / no watermark'
+echo
+echo '------------------------------------------------------------------------------------------'
+
 go run main.go -file file://../testdata/image.jpg -size 100x100,640x480,1024x768 -algo fill,width,height -times 500
 
+
 # Baseline fill operation
+echo
+echo '------------------------------------------------------------------------------------------'
+echo
+echo ' Benchmark 2: "Fill" resize type with 3 output sizes. No sharpening / no watermark'
+echo
+echo '------------------------------------------------------------------------------------------'
+
 go run main.go -file file://../testdata/image.jpg -size 100x100,640x480,1024x768 -algo fill
 
+
 # Fill with sharpening
+echo
+echo '------------------------------------------------------------------------------------------'
+echo
+echo ' Benchmark 3: "Fill" resize type with 3 output sizes. Sharpening / no watermark'
+echo
+echo '------------------------------------------------------------------------------------------'
+
 go run main.go -file file://../testdata/image.jpg -size 100x100,640x480,1024x768 -algo fill -sharpen 80
 
+
 # Fill with sharpening and watermark
+echo
+echo '------------------------------------------------------------------------------------------'
+echo
+echo ' Benchmark 4: Fill resize type with 3 output sizes. Sharpening and watermark'
+echo
+echo '------------------------------------------------------------------------------------------'
+
 go run main.go -file file://../testdata/image.jpg -size 100x100,640x480,1024x768 -algo fill -sharpen 80 -watermark file://../testdata/watermark.png
 
+
 # Fill with sharpening and watermark, small thumbnail
-go run main.go -file file://../testdata/image.jpg -size 100x100 -algo fill -sharpen 80 -watermark file://../testdata/watermark.png
+echo
+echo '------------------------------------------------------------------------------------------'
+echo
+echo ' Benchmark 5: Fill resize type with one small output size. Sharpening and watermark'
+echo
+echo '------------------------------------------------------------------------------------------'
+
+go run main.go -file file://../testdata/image.jpg -size 100x100 -algo fill -sharpen 80 -watermark file://../testdata/watermark.png -times 2000
 
